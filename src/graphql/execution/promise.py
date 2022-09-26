24cb5c6f09bc6816f9133885e4681db04e2fb153
Shen Li <dustet@gmail.com>
2022-09-26 15:51:27 -0400

    convert complete_list_value and complete_object_value to promise

--- src/graphql/execution/promise.py
+++ src/graphql/execution/promise.py
@@ -1,5 +1,16 @@
 from functools import partial
-from typing import Any, Dict, List, Optional, Type, TypeVar, Union, cast
+from typing import (
+    Any,
+    AsyncIterable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+)
 
 import promise
 from promise import Promise
@@ -9,9 +20,24 @@ from graphql.error.graphql_error import GraphQLError
 from graphql.execution.middleware import GraphQLFieldResolver
 from graphql.execution.values import get_argument_values
 from graphql.language.ast import DocumentNode, FieldNode
+from graphql.pyutils import inspect, is_iterable
 from graphql.pyutils.path import Path
 from graphql.pyutils.undefined import Undefined
-from graphql.type.definition import GraphQLObjectType, GraphQLTypeResolver
+from graphql.type.definition import (
+    GraphQLAbstractType,
+    GraphQLLeafType,
+    GraphQLList,
+    GraphQLNonNull,
+    GraphQLObjectType,
+    GraphQLOutputType,
+    GraphQLResolveInfo,
+    GraphQLTypeResolver,
+    is_abstract_type,
+    is_leaf_type,
+    is_list_type,
+    is_non_null_type,
+    is_object_type,
+)
 from graphql.type.schema import GraphQLSchema
 
 from .execute import (
@@ -20,6 +46,7 @@ from .execute import (
     Middleware,
     assert_valid_execution_arguments,
     get_field_def,
+    invalid_return_type_error,
 )
 
 T = TypeVar("T")
@@ -56,9 +83,11 @@ class PromiseExecutionContext(ExecutionContext):
                 ) -> Promise[Dict[str, Any]]:
                     def handle_results(resolved_results):
                         if is_promise(result):
+
                             def on_resolve(v):
                                 resolved_results[response_name] = v
                                 return resolved_results
+
                             return result.then(on_resolve)
                         resolved_results[response_name] = result
                         return resolved_results
@@ -207,6 +236,246 @@ class PromiseExecutionContext(ExecutionContext):
             return p
 
         return get_results()
+
+    def complete_object_value(
+        self,
+        return_type: GraphQLObjectType,
+        field_nodes: List[FieldNode],
+        info: GraphQLResolveInfo,
+        path: Path,
+        result: Any,
+    ) -> PromiseOrValue[Dict[str, Any]]:
+        """Complete an Object value by executing all sub-selections."""
+        # Collect sub-fields to execute to complete this value.
+        sub_field_nodes = self.collect_subfields(return_type, field_nodes)
+
+        # If there is an `is_type_of()` predicate function, call it with the current
+        # result. If `is_type_of()` returns False, then raise an error rather than
+        #  continuing execution.
+        if return_type.is_type_of:
+            is_type_of = return_type.is_type_of(result, info)
+
+            if self.is_promise(is_type_of):
+
+                def execute_subfields_async() -> Dict[str, Any]:
+                    is_type_of = cast(Promise, is_type_of)
+
+                    def on_is_type_of_resolve(v):
+                        if not v:
+                            raise (
+                                invalid_return_type_error(
+                                    return_type, result, field_nodes
+                                )
+                            )
+                        else:
+                            execute_result = self.execute_fields(
+                                return_type, result, path, sub_field_nodes
+                            )
+                            execute_result.then(lambda v: promise.resolve(v))
+
+                    return is_type_of.then(on_is_type_of_resolve)
+
+                return execute_subfields_async()
+
+            if not is_type_of:
+                raise invalid_return_type_error(return_type, result, field_nodes)
+
+        return self.execute_fields(return_type, result, path, sub_field_nodes)
+
+    def complete_value(
+        self,
+        return_type: GraphQLOutputType,
+        field_nodes: List[FieldNode],
+        info: GraphQLResolveInfo,
+        path: Path,
+        result: Any,
+    ) -> PromiseOrValue[Any]:
+        """Complete a value.
+
+        Implements the instructions for completeValue as defined in the
+        "Value completion" section of the spec.
+
+        If the field type is Non-Null, then this recursively completes the value
+        for the inner type. It throws a field error if that completion returns null,
+        as per the "Nullability" section of the spec.
+
+        If the field type is a List, then this recursively completes the value
+        for the inner type on each item in the list.
+
+        If the field type is a Scalar or Enum, ensures the completed value is a legal
+        value of the type by calling the ``serialize`` method of GraphQL type
+        definition.
+
+        If the field is an abstract type, determine the runtime type of the value and
+        then complete based on that type.
+
+        Otherwise, the field type expects a sub-selection set, and will complete the
+        value by evaluating all sub-selections.
+        """
+        # If result is an Exception, throw a located error.
+        if isinstance(result, Exception):
+            raise result
+
+        # If field type is NonNull, complete for inner type, and throw field error if
+        # result is null.
+        if is_non_null_type(return_type):
+            completed = self.complete_value(
+                cast(GraphQLNonNull, return_type).of_type,
+                field_nodes,
+                info,
+                path,
+                result,
+            )
+            if completed is None:
+                raise TypeError(
+                    "Cannot return null for non-nullable field"
+                    f" {info.parent_type.name}.{info.field_name}."
+                )
+            return completed
+
+        # If result value is null or undefined then return null.
+        if result is None or result is Undefined:
+            return None
+
+        # If field type is List, complete each item in the list with inner type
+        if is_list_type(return_type):
+            return self.complete_list_value(
+                cast(GraphQLList, return_type), field_nodes, info, path, result
+            )
+
+        # If field type is a leaf type, Scalar or Enum, serialize to a valid value,
+        # returning null if serialization is not possible.
+        if is_leaf_type(return_type):
+            return self.complete_leaf_value(cast(GraphQLLeafType, return_type), result)
+
+        # If field type is an abstract type, Interface or Union, determine the runtime
+        # Object type and complete for that type.
+        if is_abstract_type(return_type):
+            return self.complete_abstract_value(
+                cast(GraphQLAbstractType, return_type), field_nodes, info, path, result
+            )
+
+        # If field type is Object, execute and complete all sub-selections.
+        if is_object_type(return_type):
+            return self.complete_object_value(
+                cast(GraphQLObjectType, return_type), field_nodes, info, path, result
+            )
+
+        # Not reachable. All possible output types have been considered.
+        raise TypeError(  # pragma: no cover
+            "Cannot complete value of unexpected output type:"
+            f" '{inspect(return_type)}'."
+        )
+
+    def complete_list_value(
+        self,
+        return_type: GraphQLList[GraphQLOutputType],
+        field_nodes: List[FieldNode],
+        info: GraphQLResolveInfo,
+        path: Path,
+        result: Union[Iterable[Any], Iterable[Promise[Any]]],
+    ) -> PromiseOrValue[List[Any]]:
+        """Complete a list value.
+
+        Complete a list value by completing each item in the list with the inner type.
+        """
+        if not is_iterable(result):
+            # experimental: allow async iterables
+            if isinstance(result, AsyncIterable):
+                # This should never happen in a promise context
+                raise Exception("what's going on?")
+                # noinspection PyShadowingNames
+                async def async_iterable_to_list(
+                    async_result: AsyncIterable[Any],
+                ) -> Any:
+                    sync_result = [item async for item in async_result]
+                    return self.complete_list_value(
+                        return_type, field_nodes, info, path, sync_result
+                    )
+
+                return async_iterable_to_list(result)
+
+            raise GraphQLError(
+                "Expected Iterable, but did not find one for field"
+                f" '{info.parent_type.name}.{info.field_name}'."
+            )
+        result = cast(Iterable[Any], result)
+
+        # This is specified as a simple map, however we're optimizing the path where
+        # the list contains no coroutine objects by avoiding creating another coroutine
+        # object.
+        item_type = return_type.of_type
+        is_promise = self.is_promise
+        awaitable_indices: List[int] = []
+        append_awaitable = awaitable_indices.append
+        completed_results: List[Any] = []
+        append_result = completed_results.append
+        for index, item in enumerate(result):
+            # No need to modify the info object containing the path, since from here on
+            # it is not ever accessed by resolver functions.
+            item_path = path.add_key(index, None)
+            completed_item: PromiseOrValue[Any]
+            if is_promise(item):
+                # noinspection PyShadowingNames
+                def await_completed(item: Promise[Any], item_path: Path) -> Any:
+                    try:
+                        def on_item_resolve(item_value):
+                            completed = self.complete_value(
+                                item_type, field_nodes, info, item_path, item_value
+                            )
+                            return completed
+
+                        return item.then(on_item_resolve)
+                    except Exception as raw_error:
+                        error = located_error(
+                            raw_error, field_nodes, item_path.as_list()
+                        )
+                        self.handle_field_error(error, item_type)
+                        return None
+
+                completed_item = await_completed(item, item_path)
+            else:
+                try:
+                    completed_item = self.complete_value(
+                        item_type, field_nodes, info, item_path, item
+                    )
+                    if is_promise(completed_item):
+                        # noinspection PyShadowingNames
+                        def await_completed(item: Promise[Any], item_path: Path) -> Any:
+                            def on_error(raw_error):
+                                error = located_error(
+                                    raw_error, field_nodes, item_path.as_list()
+                                )
+                                self.handle_field_error(error, item_type)
+
+                            return item.catch(on_error)
+
+                        completed_item = await_completed(completed_item, item_path)
+                except Exception as raw_error:
+                    error = located_error(raw_error, field_nodes, item_path.as_list())
+                    self.handle_field_error(error, item_type)
+                    completed_item = None
+
+            if is_promise(completed_item):
+                append_awaitable(index)
+            append_result(completed_item)
+
+        if not awaitable_indices:
+            return completed_results
+
+        # noinspection PyShadowingNames
+        def get_completed_results() -> List[Any]:
+            def on_single_complte(index, result):
+                completed_results[index] = result
+
+            promises = [
+                completed_results[index].then(partial(on_single_complte, index))
+                for index in awaitable_indices
+            ]
+            return Promise.all(promises).then(lambda _: completed_results)
+
+        res = get_completed_results()
+        return res
 
 
 def _execute_promise(
--- tests/execution/test_execute_promise.py
+++ tests/execution/test_execute_promise.py
@@ -12,64 +12,99 @@ from graphql.type import (
 from promise import Promise
 from promise.dataloader import DataLoader
 
+from graphql.type.definition import GraphQLList
+
 
 def describe_execute_promises_handles_promises_as_return_value_from_resolvers():
+
+    author_load_stub = MagicMock()
+    name_loader_stub = MagicMock()
+    book_loader_stub = MagicMock()
+
+    class AuthorNode:
+        pass
+
+    class BookNode:
+        pass
+
+    class AuthorLoader(DataLoader):
+        def batch_load_fn(self, keys):
+            author_load_stub(keys)
+            return Promise.resolve(
+                [type(key, (AuthorNode,), {"id": key}) for key in keys]
+            )
+
+    class NameLoader(DataLoader):
+        def batch_load_fn(self, keys):
+            name_loader_stub(keys)
+            return Promise.resolve([key + " name" for key in keys])
+
+    class BooksLoader(DataLoader):
+        def batch_load_fn(self, keys):
+            book_loader_stub(keys)
+            return Promise.resolve(
+                [type(key, (BookNode,), {"id": key}) for key in keys]
+            )
+
+    author_loader = AuthorLoader()
+    name_loader = NameLoader()
+    books_loader = BooksLoader()
+
+    Book = GraphQLObjectType(
+        "Book",
+        lambda: {
+            "id": GraphQLField(GraphQLString, resolve=lambda obj, _info: obj.id),
+            "name": GraphQLField(
+                GraphQLString, resolve=lambda obj, _info: name_loader.load(obj.id)
+            ),
+            "author": GraphQLField(
+                Author,
+                resolve=lambda obj, _info: author_loader.load(obj.id.split(" ")[0]),
+            ),
+        },
+    )
+
+    Author = GraphQLObjectType(
+        "Author",
+        lambda: {
+            "id": GraphQLField(GraphQLString, resolve=lambda obj, _info: obj.id),
+            "name": GraphQLField(
+                GraphQLString, resolve=lambda obj, _info: name_loader.load(obj.id)
+            ),
+            "books": GraphQLField(
+                GraphQLList(Book),
+                resolve=lambda obj, _info: books_loader.load_many(
+                    [obj.id + f" book_{i}" for i in range(1, 3)]
+                ),
+            ),
+        },
+    )
+
+    # noinspection PyShadowingBuiltins
+    TestQuery = GraphQLObjectType(
+        "Query",
+        {
+            "author": GraphQLField(
+                Author,
+                args={"id": GraphQLArgument(GraphQLID)},
+                resolve=lambda _obj, _info, id: author_loader.load(id),
+            ),
+        },
+    )
+    TestMutation = GraphQLObjectType(
+        "Mutation",
+        {
+            "newAuthor": GraphQLField(
+                Author,
+                args={"id": GraphQLArgument(GraphQLString)},
+                resolve=lambda obj, info, id: author_loader.load(id),
+            )
+        },
+    )
+
+    TestSchema = GraphQLSchema(TestQuery, TestMutation)
+
     def executes_query():
-
-        stub = MagicMock()
-        stub2 = MagicMock()
-
-        class AuthorNode:
-            pass
-
-        class AuthorLoader(DataLoader):
-            def batch_load_fn(self, keys):
-                stub(keys)
-                return Promise.resolve(
-                    [type(key, (AuthorNode,), {"id": key}) for key in keys]
-                )
-
-        class NameLoader(DataLoader):
-            def batch_load_fn(self, keys):
-                stub2(keys)
-                return Promise.resolve([key + " name" for key in keys])
-
-        author_loader = AuthorLoader()
-        name_loader = NameLoader()
-
-        Author = GraphQLObjectType(
-            "Author",
-            lambda: {
-                "id": GraphQLField(GraphQLString, resolve=lambda obj, _info: obj.id),
-                "name": GraphQLField(
-                    GraphQLString, resolve=lambda obj, _info: name_loader.load(obj.id)
-                ),
-            },
-        )
-
-        # noinspection PyShadowingBuiltins
-        TestQuery = GraphQLObjectType(
-            "Query",
-            {
-                "author": GraphQLField(
-                    Author,
-                    args={"id": GraphQLArgument(GraphQLID)},
-                    resolve=lambda _obj, _info, id: author_loader.load(id),
-                ),
-            },
-        )
-        TestMutation = GraphQLObjectType(
-            "Mutation",
-            {
-                "newAuthor": GraphQLField(
-                    Author,
-                    args={"id": GraphQLArgument(GraphQLString)},
-                    resolve=lambda obj, info, id: author_loader.load(id),
-                )
-            },
-        )
-
-        TestSchema = GraphQLSchema(TestQuery, TestMutation)
 
         foo_query = parse(
             """
@@ -103,10 +138,12 @@ def describe_execute_promises_handles_promises_as_return_value_from_resolvers():
             None,
         )
 
-        stub.assert_called_once_with(["1", "2"])
-        stub2.assert_called_once_with(["1", "2"])
-        stub.reset_mock()
-        stub2.reset_mock()
+        author_load_stub.assert_called_once_with(["1", "2"])
+        name_loader_stub.assert_called_once_with(["1", "2"])
+
+    def execute_mutation():
+        author_load_stub.reset_mock()
+        name_loader_stub.reset_mock()
 
         foo_mutation = parse(
             """
@@ -128,12 +165,78 @@ def describe_execute_promises_handles_promises_as_return_value_from_resolvers():
             document=foo_mutation,
             execution_context_class=PromiseExecutionContext,
         ) == (
-            {
-                "a": {"id": "3", "name": "3 name"},
-                "b": {"id": "4", "name": "4 name"}
-            },
+            {"a": {"id": "3", "name": "3 name"}, "b": {"id": "4", "name": "4 name"}},
             None,
         )
 
-        stub.assert_called_once_with(["3", "4"])
-        stub2.assert_called_once_with(["3", "4"])
+        author_load_stub.assert_called_once_with(["3", "4"])
+        name_loader_stub.assert_called_once_with(["3", "4"])
+
+    def test_execute_list_results():
+        author_load_stub.reset_mock()
+        name_loader_stub.reset_mock()
+        book_loader_stub.reset_mock()
+
+        foo_query = parse(
+            """
+            query Foo {
+              a: author(id: "1") {
+                id,
+                name,
+                books {
+                    id,
+                    name,
+                    author {
+                        id
+                        name
+                        books {
+                            id,
+                            name,
+                        }
+                    }
+                }
+              }
+            }
+            """
+        )
+        assert execute_promise(
+            schema=TestSchema,
+            document=foo_query,
+            execution_context_class=PromiseExecutionContext,
+        ) == (
+            {
+                "a": {
+                    "id": "1",
+                    "name": "1 name",
+                    "books": [
+                        {
+                            "id": "1 book_1",
+                            "name": "1 book_1 name",
+                            "author": {
+                                "id": "1",
+                                "name": "1 name",
+                                "books": [
+                                    {"id": "1 book_1", "name": "1 book_1 name"},
+                                    {"id": "1 book_2", "name": "1 book_2 name"},
+                                ],
+                            },
+                        },
+                        {
+                            "id": "1 book_2",
+                            "name": "1 book_2 name",
+                            "author": {
+                                "id": "1",
+                                "name": "1 name",
+                                "books": [
+                                    {"id": "1 book_1", "name": "1 book_1 name"},
+                                    {"id": "1 book_2", "name": "1 book_2 name"},
+                                ],
+                            },
+                        },
+                    ],
+                },
+            },
+            None,
+        )
+        author_load_stub.assert_called_once_with(["1"])
+        book_loader_stub.assert_called_once_with(["1 book_1", "1 book_2"])
